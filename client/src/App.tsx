545df18d576ee0b850c98b76a93f1d812efe1cab
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-06-02 23:07:24 +0530

    Components and libraries added for Material UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,30 @@
+import { Box } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { themeSettings } from "./theme";
+import Navbar from "@/scenes/navbar";
+
+
 function App() {
-
-
-  return <div className="app"></div>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>Dashboard</div>} />
+              <Route path="/predictions" element={<div>Predictions</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
