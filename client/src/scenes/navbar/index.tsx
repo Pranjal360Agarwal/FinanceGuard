d793c17586dd99084f4217e9ebd2203230fe5ce2
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-06-03 20:20:26 +0530

    Dashboard and Predictions modules added

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import PixIcon from "@mui/icons-material/Pix";
 import { Box, Typography, useTheme } from "@mui/material";
+import FlexBetween from "@/components/FlexBetween";
 
-
+// eslint-disable-next-line @typescript-eslint/ban-types
 type Props = {};
 
-const Navbar = (props: Props) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const Navbar = (_props: Props) => {
   const { palette } = useTheme();
+  const [selected, setSelected] = useState("dashboard");
   return (
     <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
       {/* LEFT SIDE */}
