09a816390d043b283cc33fdaeb1504e0a79abdf7
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-08-01 19:15:22 +0530

    Database setup initiated

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+
+console.log("hello");
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(async() => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+        /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+        // await mongoose.connection.db.dropDatabase();
+        // KPI.insertMany(kpis);
+        // Product.insertMany(products);
+        // Transaction.insertMany(transactions);
+    })
+    .catch((error) => console.log(`${error} did not connect`));
