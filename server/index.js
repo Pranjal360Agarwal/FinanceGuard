43560e702415a657067d5e934acef98091d57706
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-08-14 19:02:44 +0530

    Products mock data added

--- server/index.js
+++ server/index.js
@@ -6,8 +6,11 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
 import KPI from "./models/KPI.js";
-import { kpis } from "./data/data.js";
+import Product from "./models/Product.js";
+import { kpis, products } from "./data/data.js";
+
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -36,7 +39,7 @@ mongoose
         /* ADD DATA ONE TIME ONLY OR AS NEEDED */
         //await mongoose.connection.db.dropDatabase();
         //KPI.insertMany(kpis);
-        // Product.insertMany(products);
+        Product.insertMany(products);
         // Transaction.insertMany(transactions);
     })
     .catch((error) => console.log(`${error} did not connect`));
