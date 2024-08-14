a7212be7e5ee8026314ebc3f043f606a32b45c50
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-08-14 23:27:11 +0530

    Prediction route added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "./theme";
 import Navbar from "@/scenes/navbar";
 import Dashboard from "@/scenes/dashboard";
-
+import Predictions from "@/scenes/predictions";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -19,7 +19,7 @@ function App() {
             <Navbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/*<Route path="/predictions" element={<Predictions />} />*/}
+              <Route path="/predictions" element={<Predictions />} />
             </Routes>
           </Box>
         </ThemeProvider>
