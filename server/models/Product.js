41fbbe19334d0f5ff942c7d19914a36067476410
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-08-14 23:31:35 +0530

    Transaction file code issues fixed

--- server/models/Product.js
+++ server/models/Product.js
@@ -4,22 +4,27 @@ import { loadType } from "mongoose-currency";
 const Schema = mongoose.Schema;
 loadType(mongoose);
 
-const ProductSchema = new Schema({
+const ProductSchema = new Schema(
+  {
     price: {
-        type: mongoose.Types.Currency,
-        currency: "USD",
-        get: (v) => v / 100,
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
     },
     expense: {
-        type: mongoose.Types.Currency,
-        currency: "USD",
-        get: (v) => v / 100,
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
     },
-    transactions: [{
+    transactions: [
+      {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Transaction",
-    }, ],
-}, { timestamps: true, toJSON: { getters: true } });
+      },
+    ],
+  },
+  { timestamps: true, toJSON: { getters: true } }
+);
 
 const Product = mongoose.model("Product", ProductSchema);
 
