63b656d024d5a627e251c16adf1133ae85267cbd
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-08-14 23:33:01 +0530

    Bugs of modules fixed

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -4,21 +4,26 @@ import { loadType } from "mongoose-currency";
 const Schema = mongoose.Schema;
 loadType(mongoose);
 
-const TransactionSchema = new Schema({
+const TransactionSchema = new Schema(
+  {
     buyer: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     amount: {
-        type: mongoose.Types.Currency,
-        currency: "USD",
-        get: (v) => v / 100,
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
     },
-    productIds: [{
+    productIds: [
+      {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Product",
-    }, ],
-}, { timestamps: true, toJSON: { getters: true } });
+      },
+    ],
+  },
+  { timestamps: true, toJSON: { getters: true } }
+);
 
 const Transaction = mongoose.model("Transaction", TransactionSchema);
 
